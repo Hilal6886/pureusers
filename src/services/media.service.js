5cddb3d72f0e023ce4daf29707235ce21f42e295
auqib <auqib.rather9@gmail.com>
2023-04-02 13:18:21 +0530

    delete service added

--- src/services/media.service.js
+++ src/services/media.service.js
@@ -1,5 +1,17 @@
+import { getStorage, ref, deleteObject } from "firebase/storage";
+
+const storage = getStorage();
+
+
 const getImageUrl=(name)=>{
     return `https://firebasestorage.googleapis.com/v0/b/quantumtourandtravels.appspot.com/o/quantumtourandtravel%2FAssets%2F${name}?alt=media`
 } 
+const deleteImage=(name)=>{
+// Create a reference to the file to delete
+const desertRef = ref(storage, name);
 
-module.exports = { getImageUrl };
+deleteObject(desertRef).then(() => {
+}).catch((error) => {
+});
+} 
+module.exports = { getImageUrl,deleteImage };
